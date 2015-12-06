10bcada04be513d0e53b8cbe7bc446345bc240da
teodorrupi <teodor.rupi@trivago.com>
2015-12-06 18:17:32 +0100

    added Streams API example class and introduction

---
+++ src/main/java/com/hl/java8/StreamExample.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2015 Humanoid Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * The first programming concept that "Java 8 in action" introduces as
+ * part of the new features that java 8 has implemented is "stream processing"
+ * via the Streams API [java.util.stream]
+ *
+ * Streams API methods can be chained to form complex pipelines to process data.
+ * The pipeline of Stream operations can be run in parallel, without the need of
+ * using Threads, on several CPU cores on disjoint parts of the input.
+ */
+
+package com.hl.java8;
+
+/**
+ * Created by trupi on 06/12/15.
+ */
+public class StreamExample {
+}
